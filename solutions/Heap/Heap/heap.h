8dd8942f6e477930e59bf5699e47ac3bdc3ecf24
Zhorin Sergey <ser4038772@mail.ru>
2016-03-06 16:08:28 +0300

    Обманул

--- solutions/Heap/Heap/heap.h
+++ solutions/Heap/Heap/heap.h
@@ -3,20 +3,20 @@
 
 class heap {
  private:
-   int size;
-   int d;
-   int* arr;
-   int n;
+  int size;
+  int d;
+  int* arr;
+  int n;
  public:
-   int min_child(int i);
-   void up(int i);
-   void down(int i);
-   heap(int _size, int _d);
-   ~heap();
-   void hilling();
-   void insert(int key);
-   int deletemin();
-   void swap(int i, int p);
+  int min_child(int i);
+  void up(int i);
+  void down(int i);
+  heap(int _size, int _d);
+  ~heap();
+  void hilling();
+  void insert(int key);
+  int deletemin();
+  void swap(int i, int p);
 };
 
 #endif // SOLUTIONS_HEAP_HEAP_HEAP_H_
